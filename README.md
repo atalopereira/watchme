5bd18ed8a4dfba013bd563e32e8e7c117b572586
atalo pereira <atalopereira@hotmail.com>
2022-05-18 20:43:40 -0300

    🎨 Improve organization of the code

---
+++ README.md
@@ -0,0 +1,17 @@
+## 💻 Challenge watchMe
+
+<img alt="watchme" title="watchme" src=".github/watchme.png" />
+
+## ✨ Technologies
+
+- React
+- Typescript
+- JSON server
+
+## 👨‍💻 Project
+
+Project that lists movies according to genre.
+
+## 🎯 Challenge
+
+The purpose of the challenge is to componentize the code that was in a single file, separating the logic from the sidebar of the movie listing logic.
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,42 @@
-export function SideBar() {
-  // Complete aqui
+import { useState, useEffect } from 'react';
+import { Button } from './Button';
+
+import { api } from '../services/api';
+
+import { GenreResponseProps } from '../@types/GenreResponse'
+
+interface SideBarProps {
+  selectedGenreId: number
+  onClickButton: (number: number) => void
+}
+
+export function SideBar({
+  selectedGenreId,
+  onClickButton
+}: SideBarProps) {
+  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
+
+  useEffect(() => {
+    api.get<GenreResponseProps[]>('genres').then(response => {
+      setGenres(response.data);
+    });
+  }, []);
+
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => onClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  )
 }
